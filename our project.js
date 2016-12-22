959b1898bac3e551f52142b584ab63cb57bf153f
KamalHajjar <kamalhajjar907@gmail.com>
2016-12-22 12:43:57 +0200

    project

---
+++ our project.js
@@ -0,0 +1,139 @@
+function disPlayBooks(){
+   var allbooks=[
+Horror1={
+Title:"Grave Remorse",
+Author:"Anthony S. Darke",
+Categories:"Horror" ,
+Year: "2016",
+downlink:"https://www.smashwords.com/books/download/689971/1/latest/0/0/grave-remorse.pdf" ,
+img: "https://dwtr67e3ikfml.cloudfront.net/bookCovers/20a22d664a8c94249d720417fa566af44021094b",
+summary:"Leanne Miller, the buxom blonde with ice in her veins and murder in her heart believes she’s pulled off her biggest score yet after taking the life of an old gypsy woman." 
+}
+,
+Horror2={
+Title:"Maestro",
+Author:" Nicholas Paschall",
+Categories:"Horror",
+Year:"2016",
+downlink:"https://www.smashwords.com/books/download/690249/1/latest/0/0/maestro.pdf",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/b354b47b6ae838e84dfcb0bd8f736cd7f980538c",
+summary:"Sarah is on her way home after a babysitting job when she is suddenly accosted by a creature of the night. Torn to pieces, she finds herself enslaved to the mad vampire, who forces her into a ghostly choir, where she begins to learn an ancient song from the favored soul. She can't control herself, and she knows she's losing more of herself as the seconds tick by" 
+}
+,
+Horror3={
+Title:"Bad Crop",
+Author:" Nicholas Paschall",
+Categories:"Horror",
+Year:"2016",
+downlink:"https://www.smashwords.com/books/download/690251/1/latest/0/0/bad-crop.pdf",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/f8c411a05a6124e23f56321d8ee66f4982413a1d",
+summary:"An inquisition is held where one brother is given his just desserts for his wicked ways, while the other is forced to watch his loved one burn for his sins before joining her in the pit." 
+}
+,
+Horror4={
+Title:"Invasion Earth",
+Author:"Philip R Benge",
+Categories:" Science",
+Year:"2015",
+downlink:"https://www.smashwords.com/books/download/591846/1/latest/0/0/invasion-earth.pdf",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/3d4f82d64be8af4113c3e2b3bcf45a9fce02f975",
+summary:"An alien race 70 light years from Earth is desperately looking for a new home world as their own is about to be destroyed by a rogue planet. The only choice is Earth, and so they decide to de-populate, and take it for themselves. A lone scientist battles to prove that the aliens are finally on Earth, unfortunately she has come to the aliens` notice, and now the fight is on, the winner takes all." 
+}
+,
+Horror5={
+Title:"Rescue Mission to the Stars",
+Author:"Philip R Benge",
+Categories:"Science",
+Year:"2013",
+downlink:"https://www.smashwords.com/books/download/324272/1/latest/0/0/rescue-mission-to-the-stars.pdf",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/b72d5788297ea502f1716f990f5b9cf94ceaeeb7",
+summary:"The victim of a hit and run accident David Charlton wakes from a coma after two years to find that his father has gone missing in the depths of space while on a survey mission. With a group of friends and with the help of his godfather he takes his father`s old freighter to search for him. " 
+}
+,
+Horror6={
+Title:"Fresh Future",
+Author:"Arsalan Pourdavar",
+Categories:"Science",
+Year:"2016",
+downlink:"https://www.smashwords.com/books/download/688624/1/latest/0/0/fresh-future.pdf",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/f8d96e4c1069f2c7af179994019a9ec25449e738",
+summary:"Fresh Future is a short science fiction novel. it tells the story of two agents who are after a group of technology thieves. " 
+}
+,
+Horror7={
+Title:"Principals of Innovation",
+Author:"Arsalan Pourdavar",
+Categories:"Science",
+Year:"2016",
+downlink:"https://www.smashwords.com/books/download/689553/1/latest/0/0/principals-of-innovation.pdf",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/f96c347d330d3d76b379f7b830b9999149c90dcc",
+summary:"in this book, we seek an answer to whether innovation is a true gift or obtainable by anyone as well. if we look at innovation as a skill and not an obtained social characteristic through time, it would be far easier to just learn it at any time of life regardless of our luck with the schools that we've already went to." 
+}
+,
+Horror8={
+Title:"Words",
+Author:"Terry Odell",
+Categories:"Romance",
+Year:"2010",
+downlink:"https://www.smashwords.com/books/download/17622/1/latest/0/0/words.pdf",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/55d360684a16b581e5b3c23624b85405f1f7a665",
+summary:"What does it take for a man to say, 'I love you' out loud? Certainly the woman he loves can tell from his actions. And if she can tell, why must he initiate those words? Words explores new love first from his point of view, then hers. A sensual read, guaranteed to make you seek out that special someone in your life." 
+}
+,
+Horror9={
+Title:"Nowhere to Hide",
+Author:"Terry Odell",
+Categories:"Romance",
+Year:"2012",
+downlink:"https://www.smashwords.com/books/download/209316/7/latest/0/1/nowhere-to-hide_20pct_sample.pdb",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/215a36bbd11d2c0ef1bde945d0e182a7bab997c6",
+summary:"After police officer Colleen McDonald is injured in a case gone south and her partner dies, she leaves her job and friends in small town Oregon for a fresh start as a civilian in Orlando. When a deputy comes to her door investigating a missing person, she’s determined to get him out of her life by beating him at his own game." 
+}
+,
+Horror10={
+Title:"Love is...Blind",
+Author:"Gabriela Louis",
+Categories:"Romance",
+Year:"2016",
+downlink:"https://www.smashwords.com/books/download/648385/1/latest/0/0/love-isblind.pdf",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/0fb16f0f26df10396ddd2cec91ac953de3889b84",
+summary:"Josie is a single mother struggling to raise her daughter by herself. When the police find a dead body at her mother’s house, Jerome, a burned out cop, and unattached neat freak lets them stay at his apartment for their protection. Will he be able to solve the crime and protect Josie and her daughter, or is he the one who needs protecting from them" 
+}
+,
+Horror11={
+Title:"Trick or Treat",
+Author:"Rosemary J. Peel",
+Categories:"Children",
+Year:"2015",
+downlink:"https://www.smashwords.com/books/download/588581/1/latest/0/0/trick-or-treat.pdf",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/f6011770117e99667afd06723a64359c45e8e588",
+summary:"A Halloween special :Evie’s Halloween begins with disappointment as one by one her friends let her down. Finally though it turns out the most memorable ever!" 
+}
+,
+Horror12={
+Title:"Sands of Time",
+Author:"Rosemary J. Peel",
+Categories:"Children",
+Year:"2015",
+downlink:"https://www.smashwords.com/books/download/597767/1/latest/0/0/sands-of-time.pdf",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/aa1e092a4a40eefc1fdf77618e64f470ff665cf2",
+summary:"A sequel to Trick or Treat. Many Halloweens have past since Evie first met ghost children Meg and George. She is now grown up and married. They of course remain forever children. The story continues when something draws her to the Memorial Park on 31st October 2015. Friendships are renewed and revelations revealed." 
+}
+,
+Horror13={
+Title:"Bianca",
+Author:"Rosemary J. Peel",
+Categories:"Children",
+Year:"2015",
+downlink:"https://www.smashwords.com/books/download/515925/1/latest/0/0/bianca.pdf",
+img:"https://dwtr67e3ikfml.cloudfront.net/bookCovers/75f75b0378185dd7c8ca93a8257de52ed0e666d3",
+summary:"Continuing the story of Freddie the Earth Giant. Bianca, the youngest of the Cookson children, is worried about her big friend. She senses that despite being accepted by the people of Bodwellington he remains lonely. When he sets out to explore The Wilderness, a vast underground wasteland Bianca foolishly follows." 
+}
+]
+   var  soso=[]
+       for (var i = 0; i <allbooks.length; i++) {
+          soso.push(allbooks[i].Title)
+       }
+       
+       return soso
+}
---
+++ test.js
@@ -0,0 +1,11 @@
+function last (arr){
+    
+    var x=document.getElementByTagName('h1')[0] ;
+x.textContent=arr[arr.length-1]
+} 
+arr2=[1,2,3]
+last(arr2);
+
+function go() {
+ document.write("Hello World!");
+}
